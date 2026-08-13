fdd59007c2fd3f7fb6d545712e76142769f2d8a1
fhhbbdhvd123vch-max <fhhbbdhvd123vch@gmail.com>
2026-08-14 01:32:51 +0300

    Configure Next.js

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com', 'via.placeholder.com'],
+  },
+  headers: async () => [
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'X-DNS-Prefetch-Control',
+          value: 'on'
+        },
+      ],
+    },
+  ],
+}
+
+module.exports = nextConfig
